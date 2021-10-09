822e384b7ec55c7f93b417c1541dca85c099aaeb
JCI <claeo3@gmail.com>
2021-10-09 22:00:24 +0900

    3 funtions

---
+++ main.c
@@ -0,0 +1,7 @@
+int	rush(int width, int height);
+
+int	main(void)
+{
+	rush(10, 5);
+	return (0);
+}
--- rush.c
+++ rush.c
@@ -1,18 +1,4 @@
-#include <unistd.h>
-
-void	ft_putchar(char c);
-int	rush(int width, int height);
-
-int	main(void)
-{
-	rush(5, 5);
-	return (0);
-}
-
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void ft_putchar(char c);
 
 int 	rush(int width, int height)
 {
@@ -20,42 +6,29 @@ int 	rush(int width, int height)
 	int y;
 
 	if (width <= 0 || height <= 0)
-	{
-		ft_putchar('\n');
 		return (0);
-	}
 	y = 1;
 	while (y <= height)
 	{
 		x = 1;
 		if (y == 1 || y == height)
-		{
 			while (x <= width)
+			{
 				if (x == 1 || x == width)
-				{
 					ft_putchar('o');
-					x++;
-				}
 				else
-				{
 					ft_putchar('-');
-					x++;
-				}
-		}
+				x++;
+			}
 		else
 		{
 			while (x <= width)
 			{
 				if ( x == 1 || x == width)
-				{
 					ft_putchar('|');
-					x++;
-				}
 				else
-				{
 					ft_putchar(' ');
-					x++;
-				}
+				x++;
 			}
 		}
 		ft_putchar('\n');
