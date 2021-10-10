336203de9de5e8f2d07d39f741650000749dec24
Changin Jeong <chanjeon@c5r7s7.42seoul.kr>
2021-10-10 13:59:47 +0900

    RUSH00

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -1,38 +1,29 @@
-void ft_putchar(char c);
+void	ft_putchar(char c);
 
-int 	rush(int x, int y)
+int	rush(int x, int y)
 {
-	int width;
-	int height;
+	int	width;
+	int	height;
 
 	if (x <= 0 || y <= 0)
 		return (0);
-	height = 1;
-	while (height <= y)
+	height = 0;
+	while (++height <= y)
 	{
-		width = 1;
+		width = 0;
 		if (height == 1 || height == y)
-			while (width <= x)
-			{
+			while (++width <= x)
 				if (width == 1 || width == x)
 					ft_putchar('o');
-				else
-					ft_putchar('-');
-				width++;
-			}
 		else
-		{
-			while (width <= x)
-			{
-				if ( width == 1 || width == x)
+			ft_putchar('-');
+		else
+			while (++width <= x)
+				if (width == 1 || width == x)
 					ft_putchar('|');
-				else
-					ft_putchar(' ');
-				width++;
-			}
-		}
+		else
+			ft_putchar(' ');
 		ft_putchar('\n');
-		height++;
 	}
 	return (0);
 }
